051102c05eea9dc2d7a5a9870f8843566ce8ac3a
HARIHARAN853 <hariharan853k@gmail.com>
2024-10-24 09:50:32 +0530

    Java File

---
+++ demo.java
@@ -0,0 +1,22 @@
+package New;
+
+         class Demo {
+
+                static int a,b,c;
+                static {
+                a = 100;
+                b = 200;
+                c =300;
+                }
+                static void display() {
+                System.out.println(a);
+                System.out.println(b);
+                System.out.println(c);
+                }
+
+                public static void main(String[] args) {
+                Demo.display();
+
+                }
+        }
+
